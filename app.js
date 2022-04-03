c093d1934a0dd629c9434ee053e011278adba508
vasanthofficial <vasanthdn7@gmail.com>
2022-04-03 12:50:31 +0530

    NUMBER FOUR

---
+++ app.js
@@ -0,0 +1,10 @@
+let init = prompt("ENTER THE COMMENT");
+const tasks = ["COLT", "LOOPS"];
+console.log(tasks);
+let newList;
+while (init !== "quit" && init !== "q") {
+  init = prompt("ENTER THE COMMENT");
+  if (init === "list") {
+    console.log("**********");
+  }
+}
